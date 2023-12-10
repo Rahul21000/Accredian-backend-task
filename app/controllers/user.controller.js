a43953681d71b36658e9233107a8b2177cf9ed1f
rahulAtiuttam <128784944+Rahul21000@users.noreply.github.com>
2023-12-11 00:36:21 +0530

    commit4

--- app/controllers/user.controller.js
+++
@@ -1,9 +0,0 @@
-  
-  exports.allAccess = (req, res) => {
-    res.status(200).send("Public Content.");
-  };
-  exports.dashborad = (req, res) => {
-    res.status(200).send("User Dashboard.");
-  };
-  
- 
